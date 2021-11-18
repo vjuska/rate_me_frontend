ddaf4248c87978694d88a7bda86cfea2ba9efdf1
Vic <vjuska24@gmail.com>
2021-11-18 14:27:41 -0600

    fixed bug

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://hidden-thicket-65529.herokuapp.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://hidden-thicket-65529.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
